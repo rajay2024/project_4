631ac3ffda93f17a95662bda486ed66515930cdf
rajay2024 <rajay.sae.comp@gmail.com>
2024-02-16 19:23:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('this is a sample for cicd pipeline')
+})
+
+app.listen(port, () => {
+    console.log('Application is listening at http://localhost:${port}')
+})
